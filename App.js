6b799d248fc7d61a738083738ec1888f0691a1d4
Justin Hamerly <suvimountain@icloud.com>
2022-12-29 13:20:31 -0800

    added basic styles to differentiate header

--- App.js
+++ App.js
@@ -4,9 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Header - navbar drawer, logo, login/logout</Text>
-      <Text>Game Interface</Text>
-      
+      <View style={styles.header}>
+        <Text style={styles.headerText}>
+          Header - navbar drawer, logo, login/logout
+        </Text>
+      </View>
+      <View>
+        <Text style={styles.bodyText}>
+          Game Interface
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,4 +27,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#65278E',
+    width: '100%',
+    height: '10%',
+  },
+  headerText: {
+    fontSize: 25
+  },
+  bodyText: {
+    fontSize: 20
+  }
 });
--- Header.js
+++ Header.js
@@ -2,3 +2,30 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
+export default function Header() {
+  return(
+    <View style={styles.header}>
+      <View>
+        <Text style={styles.headerText}>
+          Overboss Companion
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headerTexxt: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'black',
+    letterSpacing: 1
+  }
+})
